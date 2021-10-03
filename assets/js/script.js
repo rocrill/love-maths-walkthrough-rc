834d63e21c6f33340005656fc17d2a919b80dfeb
rocrill <crillyr@tcd.ie>
2021-10-03 16:03:26 +0000

    Disaply addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+    runGame("addition");
 });
 
 /**
@@ -48,9 +49,14 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 }
+
 
 function displaySubtractQuestion() {
 
